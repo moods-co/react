e1e2c1046d85e5cd5cbf0b8909649d60f0b2bd02
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2025-03-29 13:54:37 +0100

    Cleaning - but sill broken

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
+import './styles/index.css';
 import App from './App';
-//import { Provider } from 'react-redux';
-//import playlistReducer from './stores/playlist';
-//import { configureStore } from '@reduxjs/toolkit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
